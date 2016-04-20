72b1681becc971ed74a4cb66e1afcdf4093eb83e
Kai Wetlesen <kaiw@fiona.kaiwetlesen.com>
2016-04-20 14:47:49 -0700

    Rest of the file tree

---
+++ application/js/empty.js
@@ -0,0 +1,8 @@
+/*
+ * Organize the js similar to how we do in the css
+ * folder! Library JS goes into a folder, organized
+ * by publisher.
+ *
+ * This folder is accessible by:
+ * http://parkmeister.kaiw.dk/js/
+ */
---
+++ table_definitions/empty.sql
@@ -0,0 +1,6 @@
+/*
+All tables shall be stored in .sql files.
+Once a table has been installed, all further
+alterations must be made using ALTER TABLE
+statements.
+*/
