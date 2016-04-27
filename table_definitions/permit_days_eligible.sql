fe6a97830e8d62a539b49470ea9d5d015f416ea2
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-04-27 10:42:03 -0700

    Added permit_days_eligible

---
+++ table_definitions/permit_days_eligible.sql
@@ -0,0 +1,13 @@
+CREATE TABLE permit_days_eligible (
+	permit_type CHAR(25) NOT NULL COMMENT 'Refers to permit table.',
+	provided_by CHAR(25) COMMENT 'Refers to parking operator table.',
+	day_eligible ENUM('SUN','MON','TUE','WED','THU','FRI','SAT'),
+	PRIMARY KEY(permit_type, provided_by)
+) ENGINE=InnoDb;
+
+INSERT INTO permit_days_eligible (permit_type, provided_by, day_eligible) VALUES
+	('Fall 2016 Weekday Only', 'San Jose State University', 'MON'),
+	('Fall 2016 Weekday Only', 'San Jose State University', 'TUE'),
+	('Fall 2016 Weekday Only', 'San Jose State University', 'WED'),
+	('Fall 2016 Weekday Only', 'San Jose State University', 'THU'),
+	('Fall 2016 Weekday Only', 'San Jose State University', 'FRI');
