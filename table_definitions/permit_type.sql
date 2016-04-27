9e099b4cb11bdd4207b9f2a85dad80db652ee0b0
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-04-27 13:42:04 -0700

    Updated some properties with the example data

--- table_definitions/permit_type.sql
+++ table_definitions/permit_type.sql
@@ -8,7 +8,8 @@ CREATE TABLE permit_type (
 
 -- An example entry might look like:
 INSERT INTO permit_type(permit_type, provided_by, effective_on, expires_on) VALUES
-	('Fall 2016 Weekday Only', 'San Jose State University', '01-01-2016', '06-01-2016');
+	('Fall 2016', 'San Jose State University', '01-01-2016', '06-01-2016');
 
+-- Or this:
 INSERT INTO permit_type(permit_type, provided_by, effective_on, expires_on) VALUES
 	('Fall 2016 Weekday Only', 'San Jose State University', '01-01-2016', '06-01-2016');
