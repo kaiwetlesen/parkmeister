d78eb6455415093e35da80ff7f0a69e8b130af0f
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-04-27 13:54:05 -0700

    Added example data files

--- table_definitions/permit_days_eligible.sql
+++ table_definitions/permit_days_eligible.sql
@@ -4,11 +4,3 @@ CREATE TABLE permit_days_eligible (
 	day_eligible ENUM('SUN','MON','TUE','WED','THU','FRI','SAT'),
 	PRIMARY KEY(permit_type, provided_by)
 ) ENGINE=InnoDb;
-
--- Example data
-INSERT INTO permit_days_eligible (permit_type, provided_by, day_eligible) VALUES
-	('Fall 2016 Weekday Only', 'San Jose State University', 'MON'),
-	('Fall 2016 Weekday Only', 'San Jose State University', 'TUE'),
-	('Fall 2016 Weekday Only', 'San Jose State University', 'WED'),
-	('Fall 2016 Weekday Only', 'San Jose State University', 'THU'),
-	('Fall 2016 Weekday Only', 'San Jose State University', 'FRI');
--- table_definitions/permit_type.sql
+++ table_definitions/permit_type.sql
@@ -5,11 +5,3 @@ CREATE TABLE permit_type (
 	expires_on DATE DEFAULT NULL COMMENT 'Date that the parking pass expires.',
 	PRIMARY KEY(permit_type, provided_by)
 ) ENGINE=InnoDb;
-
--- An example entry might look like:
-INSERT INTO permit_type(permit_type, provided_by, effective_on, expires_on) VALUES
-	('Fall 2016', 'San Jose State University', '01-01-2016', '06-01-2016');
-
--- Or this:
-INSERT INTO permit_type(permit_type, provided_by, effective_on, expires_on) VALUES
-	('Fall 2016 Weekday Only', 'San Jose State University', '01-01-2016', '06-01-2016');
