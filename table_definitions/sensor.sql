fb18e1b1c39d8dbc362437031d8d792ca33940e4
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-04-27 20:19:16 -0700

    Fixed things

--- table_definitions/sensor.sql
+++ table_definitions/sensor.sql
@@ -1,4 +1,4 @@
-CREATE TABLE sensor_group (
+CREATE TABLE sensor (
 	serial INTEGER COMMENT '64 bit serial number of the sensor.',
 	sensor_type ENUM ( 'PROXIMITY', 'INGRESS', 'EGRESS' ),
 	-- TODO: Figure out what the pkey datatype will be for parking lots
