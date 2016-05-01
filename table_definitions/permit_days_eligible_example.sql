ca7a4b8fe34b08b8415722233980f7141f58a7af
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-05-01 15:45:32 -0700

    Added multi-value attribute days_eligible

---
+++ table_definitions/permit_days_eligible_example.sql
@@ -0,0 +1,3 @@
+-- Example data
+INSERT INTO permit_days_eligible (permit_type, provided_by, eligible_sat, eligible_sun) VALUES
+	('Fall 2016 Weekday Only', 'San Jose State University', FALSE, FALSE);
---
+++ table_definitions/sensor.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sensor (
+	serial INTEGER COMMENT '64 bit serial number of the sensor.',
+	sensor_type ENUM ( 'PROXIMITY', 'INGRESS', 'EGRESS' ),
+	-- TODO: Figure out what the pkey datatype will be for parking lots
+	lot_id INTEGER COMMENT 'Associates sensors with a particular lot.',
+	PRIMARY KEY ( serial )
+) ENGINE=InnoDB;
