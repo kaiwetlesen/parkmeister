e7976f879f19514df72dbfef4ea0e3fa891ac6b7
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-05-01 17:49:50 -0700

    Added a boolean to indicate sensor status

--- table_definitions/sensor.sql
+++ table_definitions/sensor.sql
@@ -3,5 +3,6 @@ CREATE TABLE sensor (
 	sensor_type ENUM ( 'PROXIMITY', 'INGRESS', 'EGRESS' ),
 	-- TODO: Figure out what the pkey datatype will be for parking lots
 	lot_id INTEGER COMMENT 'Associates sensors with a particular lot.',
+	is_occupied BOOLEAN COMMENT 'Indicates whether a sensor sees a car. Only referenced for proximity sensors.',
 	PRIMARY KEY ( serial )
 ) ENGINE=InnoDB;
