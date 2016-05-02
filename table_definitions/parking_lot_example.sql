d71c015f7ff97c05fd09aeb0f5b0eb1760029892
SandmanDP <bj.wahhab@gmail.com>
2016-05-01 17:07:12 -0700

    Added files via upload

--- table_definitions/parking_lot_example.sql
+++ table_definitions/parking_lot_example.sql
@@ -1,3 +1,3 @@
 -- An example entry might look like:
 INSERT INTO parking_lot(geo_lat, geo_long, lot_name, lot_type) VALUES
-	(24.0000, 63.0000, 'Test lot', 'Test type');
+	(24.0000, 63.0000, 'Test lot', 'Street Parking');
