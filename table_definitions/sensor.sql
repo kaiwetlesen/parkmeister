189f17e96e4cc4ef8079ce31e540fe4b8cab9acd
timmag <timm91@hotmail.com>
2016-05-02 17:14:43 -0700

    removed a word (git test)
    better description of changes

--- table_definitions/sensor.sql
+++ table_definitions/sensor.sql
@@ -1,5 +1,5 @@
 CREATE TABLE sensor (
-	serial INTEGER COMMENT '64 bit serial number of the sensor.',
+	serial INTEGER COMMENT '64 bit serial of the sensor.',
 	sensor_type ENUM ( 'PROXIMITY', 'INGRESS', 'EGRESS' ),
 	-- TODO: Figure out what the pkey datatype will be for parking lots
 	lot_id INTEGER COMMENT 'Associates sensors with a particular lot.',
