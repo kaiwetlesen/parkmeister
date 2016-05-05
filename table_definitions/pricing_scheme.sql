99c8bce56f8fcd159978e9f279645b747fb50d03
timmag <timm91@hotmail.com>
2016-05-05 16:40:23 -0700

    Changed price_per_interval from int to float

--- table_definitions/pricing_scheme.sql
+++ table_definitions/pricing_scheme.sql
@@ -1,7 +1,7 @@
 CREATE TABLE pricing_scheme(
 	payment_type varchar(10) NOT NULL,
     pass_required varchar(3),
-    price_per_interval int,
+    price_per_interval float,
     parking_interval date,
     PRIMARY KEY(payment_type) );
     
