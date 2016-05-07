168b68f221b1770a89106e00bc912b25dbdf2a32
Kai Wetlesen <kaiw@fiona.kaiwetlesen.com>
2016-05-06 19:41:17 -0700

    Fixed circular dependencies

--- table_definitions/parking_operator.sql
+++ table_definitions/parking_operator.sql
@@ -3,18 +3,15 @@ use acme;
 /* describe test;
 */
 create table parking_operator (
-	#operator_information
+	-- operator_information
 	operator_id int primary key auto_increment, 
     operator_name varchar (48),
     address varchar (160),
-    #contact_information
+    -- contact_information
     contact_num int, 
     contact_site varchar(160),
-    #billing_info
+    -- billing_info
     billing_amount int,
     billing_type varchar(48),
     lot_id int NOT NULL,
-    owned_sensors int,
-    FOREIGN KEY(lot_id) REFERENCES parking_lot(lot_id),
-    FOREIGN KEY(owned_sensors) REFERENCES sensor(serial)
     )ENGINE = innoDb;
