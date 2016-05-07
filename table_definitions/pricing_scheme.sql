ac570046dec6a97e664fadd3634b71cc11e03b99
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-05-06 18:51:29 -0700

    Reverted to an integer, see comment

--- table_definitions/pricing_scheme.sql
+++ table_definitions/pricing_scheme.sql
@@ -1,7 +1,7 @@
 CREATE TABLE pricing_scheme(
 	payment_type varchar(10) NOT NULL,
     pass_required varchar(3),
-    price_per_interval float,
+    price_per_interval integer comment "This field will be represented using fixed integers representing 2 decimal numbers.",
     parking_interval date,
     PRIMARY KEY(payment_type) );
     
