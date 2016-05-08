560ec0c1c03243e54dd1ed708ff6e0d3c541847c
johlell42 <ianjohlell@gmail.com>
2016-05-08 16:02:44 -0700

    Update account.sql

--- table_definitions/account.sql
+++ table_definitions/account.sql
@@ -1,5 +1,5 @@
 CREATE TABLE account (
-  account_id int unsigned NOT NULL auto_increment,
+  account_num int unsigned NOT NULL auto_increment,
   account_name CHAR(25),
   account_email varchar(256) NOT NULL UNIQUE comment "email address used for login",
   account_password varchar(515),
