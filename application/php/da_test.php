4bd53ef31ae648810f57cdea01efac19e490f2bc
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-05-08 15:38:19 -0700

    asdfsd

--- application/php/da_test.php
+++ application/php/da_test.php
@@ -5,9 +5,9 @@ require "DriverAccountMapper.php";
 $db = new DbConnection();
 
 $db->query("use acme");
-$stmt = $db->prepare("show tables");
-$stmt->execute();
-print_r($stmt->fetchAll());
+#$stmt = $db->prepare("show tables");
+#$stmt->execute();
+#print_r($stmt->fetchAll());
 $da_mapper = new DriverAccountMapper($db);
 
 echo "Something worked!\n";
