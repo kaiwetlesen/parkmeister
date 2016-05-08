1b91deb67dceec4a28dd398e267574651edddd63
johlell42 <ianjohlell@gmail.com>
2016-05-08 15:09:04 -0700

    Update account.sql

--- table_definitions/account.sql
+++ table_definitions/account.sql
@@ -2,6 +2,6 @@ CREATE TABLE account (
   account_num int unsigned NOT NULL auto_increment,
   account_name CHAR(25),
   account_email varchar(256) NOT NULL UNIQUE comment "email address used for login",
-  account_pass varchar(515),
+  account_password varchar(515),
   PRIMARY KEY (account_num)
 )ENGINE = InnoDb;
