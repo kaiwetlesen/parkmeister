c9ee4a7060ebdde875ac6d527bccae48e36ebc60
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-05-08 12:31:45 -0700

    corrected, was working with the wrong objects

--- application/php/da_test.php
+++ application/php/da_test.php
@@ -5,9 +5,9 @@ require "DriverAccountMapper.php";
 $db = new DbConnection();
 
 $db->query("use acme");
-$db->prepare("show tables");
-$db->execute();
-print_r($db->fetchAll());
+$stmt = $db->prepare("show tables");
+$stmt->execute();
+print_r($stmt->fetchAll());
 $da_mapper = new DriverAccountMapper($db);
 
 echo "Something worked!\n";
