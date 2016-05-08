6669fdf19f40c85f7a7ff48b0f7d237e29acc1c7
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-05-08 12:31:04 -0700

    Testing out the DB handle

--- application/php/da_test.php
+++ application/php/da_test.php
@@ -3,6 +3,11 @@ require "DbConnection.php";
 require "DriverAccountMapper.php";
 
 $db = new DbConnection();
+
+$db->query("use acme");
+$db->prepare("show tables");
+$db->execute();
+print_r($db->fetchAll());
 $da_mapper = new DriverAccountMapper($db);
 
 echo "Something worked!\n";
