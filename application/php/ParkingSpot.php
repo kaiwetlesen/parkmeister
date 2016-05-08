100607ba16335a0e9bd0171eabf1dab14e3b3039
johlell42 <ianjohlell@gmail.com>
2016-05-07 23:30:38 -0700

    Added files via upload

---
+++ application/php/ParkingSpot.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * ParkingSpot
+ * Container class for a ParkingSpot entity. Maps over
+ * to the parking_struct table using ParkingSpotMapper.
+ */
+
+
+class ParkingSpot extends ParkingStruct{
+	/**private $lot_id;
+	private $geo_lat;
+	private $lot_name;
+	private $operator_id;*/
+	private $OperatorName;
+	private $Available;
+	private $SpotNum;
+
+	public function opName($on){ // operator_name, forieghn key
+		if (is_string($on)){
+			$self->OperatorName= $on;
+		}
+	}
+
+	public function Available($av){ // 
+		if (is_bool($av)){
+			$self->Available= $av;
+		}
+		return $self->Available;
+	}
+
+	public function SpotNum($sn){ // 
+		if (is_int($sn)){
+			$self->SpotNum= $sn;
+		}
+		return $self->SpotNum;
+	}
+
+	public function to_json() { // json_encode
+		return json_encode(Array(
+				'OperatorName' => $this->OperatorName (),
+				'Available' => $this->Available (),
+				'SpotNum' => $this->SpotNum ()
+				
+			));
+	}
+
+	public function from_json() {   // jason_decode
+		return json_decode(Array(
+				'OperatorName' => $this->OperatorName (),
+				'Available' => $this->Available (),
+				'SpotNum' => $this->SpotNum ()
+
+			));
+	}
+
+
+}
+?>
---
+++ application/php/ParkingStruct.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * ParkingStruct or parking lot
+ * Container class for a ParkingStruct entity. Maps over
+ * to the parking_struct table using ParkingStructMapper.
+ */
+
+
+class ParkingStruct {
+	private $lot_id;
+	private $geo_lat;
+	private $geo_long;
+	private $lot_name;
+	private $operator_id;
+
+	public function lot_id($li){ // id of the parking lot
+		if (is_int($li)){
+			$self->lot_id= $li;
+		}
+	}
+
+	public function geo_lat($lat){ // geo latitude, east to west
+		if (is_float($lat)){
+			$self->geo_lat= $lat;
+		}
+	}
+
+	public function geo_long($lon){ // geo longitude, north to south
+		if (is_float($lon)){
+			$self->geo_long= $lon;
+		}
+	}
+
+	public function lot_name($nam){ // name of parkng structure or lot
+		if (is_string($nam)){
+			$self->lot_name= $nam;
+		}
+	}
+
+	public function to_json() { // json_encode
+		return json_encode(Array(
+				'lot_id' => $this->lot_id (),
+				'geo_lat' => $this->geo_lat (),
+				'geo_long' => $this->geo_long (),
+				'lot_name' => $this->lot_name ()
+			));
+	}
+
+	public function from_json() {   // jason_decode
+		return json_decode(Array(
+				'lot_id' => $this->lot_id (),
+				'geo_lat' => $this->geo_lat (),
+				'geo_long' => $this->geo_long (),
+				'lot_name' => $this->lot_name ()
+
+			));
+	}
+
+
+
+
+}
+?>
