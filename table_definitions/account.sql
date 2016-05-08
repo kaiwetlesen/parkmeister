51f3added172624d104da1c22ab94a48fd4c868d
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-05-08 11:40:36 -0700

    Revised d.t. for email, wasn't NOT NULL UNIQUE

--- table_definitions/account.sql
+++ table_definitions/account.sql
@@ -1,7 +1,7 @@
 CREATE TABLE account (
   account_num int unsigned NOT NULL auto_increment,
   account_name CHAR(25),
-  account_email varchar(256) comment "email address used for login",
+  account_email varchar(256) NOT NULL UNIQUE comment "email address used for login",
   account_pass varchar(515),
   PRIMARY KEY (account_num)
 )ENGINE = InnoDb;
