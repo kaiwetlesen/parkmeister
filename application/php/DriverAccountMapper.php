f424d13a0947a09e7e462eb3fcfc61e87ed4cb17
Kai M. Wetlesen <kmwetles@us.ibm.com>
2016-05-08 15:44:21 -0700

    asdfasdf

--- application/php/DriverAccountMapper.php
+++ application/php/DriverAccountMapper.php
@@ -13,7 +13,7 @@ class DriverAccountMapper extends MapperBase {
 	
 	private $q_searchByAcctNum = "";
 
-	public function __construct(DbConnection $db) {
+	public function __construct(object $db) {
 		parent::__construct($db);
 	}
 
