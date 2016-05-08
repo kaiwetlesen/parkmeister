62e051c108cb9f492d7bee0862c2a601b06dec88
Fernando Henrique Silva Abolafio <fernandoabolafio@Fernandos-MacBook-Pro-7.local>
2016-05-08 12:39:43 -0700

    signup is generating the Json object

--- application/js/index.js
+++ application/js/index.js
@@ -1,3 +1,35 @@
+var passwordsMatches = function(p1,p2){
+  if (p1==p2){
+    return true;
+  }
+  return false;
+}
+function signUp () {
+  var email = $("#inputEmail").val();
+	var password = $("#inputPassword").val();
+	var confirmPassword = $("#confirmPassword").val();
+  var firstName = $("#inputName").val();
+  var lastName = $("#inputLastName").val();
+  if (passwordsMatches(password, confirmPassword)) {
+    console.log("password Ok");
+    var form = '{ "operation":"create",' +
+    '"entity":"account",' +
+    '"params": { '+
+    '"firstName":'+'"'+firstName+" "+lastName+'",'+
+    '"email":'+'"'+email+'",'+
+    '"password":'+'"'+password+'"'+
+    '}}'
+    console.log(form);
+    var obj = JSON.parse(form);
+    console.log(obj);
+
+    //TODO: create server request to signup
+  }else{
+    console.log("password doesnt match");
+  }
+
+}
+
 var main = function () {
 
 
@@ -7,12 +39,18 @@ var main = function () {
     var region = response.region;
     var postal = response.postal;
     var location = response.loc;
-    console.log(ip + city + region);
-    console.log(JSON.stringify(response, null, 4));
+    //console.log(ip + city + region);
+    //console.log(JSON.stringify(response, null, 4));
     $("#address").html(city + ", " + region);
     $("#postal").html(postal);
 
   }, "jsonp");
+
+
+  $("#signup").submit(function(e){
+    e.preventDefault();
+    signUp();
+  })
 
 
 
