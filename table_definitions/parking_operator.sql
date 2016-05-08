12469e03535fae36c86acb1bb1b303888aec5fac
SandmanDP <bj.wahhab@gmail.com>
2016-05-08 16:16:01 -0700

    Update parking_operator.sql

--- table_definitions/parking_operator.sql
+++ table_definitions/parking_operator.sql
@@ -7,6 +7,8 @@ create table parking_operator (
 	operator_id int unsigned primary key auto_increment, 
     operator_name varchar (48),
     address varchar (160),
+    account_email varchar(256) NOT NULL UNIQUE,
+    account_password varchar(515),
     -- contact_information
     contact_num varchar(11), 
     contact_site varchar(160),
