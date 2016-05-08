3768243b9c79dc566cfe64fda511e0f11a5bdcd1
johlell42 <ianjohlell@gmail.com>
2016-05-07 18:21:47 -0700

    Update pricing_scheme.sql

--- table_definitions/pricing_scheme.sql
+++ table_definitions/pricing_scheme.sql
@@ -2,6 +2,6 @@ CREATE TABLE pricing_scheme(
 	payment_type varchar(12) NOT NULL,
     pass_required varchar(3),
     price_per_interval integer comment "This field will be represented using fixed integers representing 2 decimal numbers.",
-    parking_interval date,
+    parking_interval int,
     PRIMARY KEY(payment_type) );
     
