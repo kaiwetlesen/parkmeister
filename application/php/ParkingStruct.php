54a4e4ce4253cd30d7e0ec37c9658ade121ff644
johlell42 <ianjohlell@gmail.com>
2016-05-08 18:07:15 -0700

    Update ParkingStruct.php

--- application/php/ParkingStruct.php
+++ application/php/ParkingStruct.php
@@ -12,29 +12,34 @@ class ParkingStruct {
 	private $geo_long;
 	private $lot_name;
 	private $operator_id;
+	private $lot_type;
 
 	public function lot_id($li){ // id of the parking lot
 		if (is_int($li)){
 			$this->lot_id= $li;
 		}
+		return $this->lot_id;
 	}
 
 	public function geo_lat($lat){ // geo latitude, east to west
 		if (is_float($lat)){
 			$this->geo_lat= $lat;
 		}
+		return $this->geo_lat;
 	}
 
 	public function geo_long($lon){ // geo longitude, north to south
 		if (is_float($lon)){
 			$this->geo_long= $lon;
 		}
+		return $this->geo_long;
 	}
 
 	public function lot_name($nam){ // name of parkng structure or lot
 		if (is_string($nam)){
 			$this->lot_name= $nam;
 		}
+		return $this->lot_name;
 	}
 
 	public function opID($oi){ // 
@@ -43,13 +48,21 @@ class ParkingStruct {
 		}
 		return $this->operator_id;
 	}
+	
+	public function lot_type($lt){ // id of the parking lot
+		if (is_string($lt)){
+			$this->lot_name= $lt;
+		}
+		return $this->lot_name;
+	}
 
 	public function to_json() { // json_encode
 		return json_encode(Array(
 				'lot_id' => $this->lot_id (),
 				'geo_lat' => $this->geo_lat (),
 				'geo_long' => $this->geo_long (),
-				'lot_name' => $this->lot_name ()
+				'lot_name' => $this->lot_name (),
+				'lot_type' => $this->lot_type ()
 			));
 	}
 
@@ -58,7 +71,8 @@ class ParkingStruct {
 				'lot_id' => $this->lot_id (),
 				'geo_lat' => $this->geo_lat (),
 				'geo_long' => $this->geo_long (),
-				'lot_name' => $this->lot_name ()
+				'lot_name' => $this->lot_name (),
+				'lot_type' => $this->lot_type ()
 
 			));
 	}
